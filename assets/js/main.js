e7f7f55145bd76be7844c1b3852ff0a9245ef871
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-05-16 22:24:09 +0300

    Fix linters errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 const menubtn = document.querySelector('.bx-menu');
-const closeBtn = document.querySelector('.close-btn');
+const closeBtn = document.querySelector('.close-btn-menu');
 const sideMenu = document.querySelector('.side-menu');
 const menuItems = document.querySelectorAll('.side-menu ul li');
 const detailBackdrop = document.querySelector('.backdrop');
-const desc_modal = document.querySelector('.modal');
+const descModal = document.querySelector('.modal');
 const projectCard = document.querySelectorAll('.project-card');
 const closebtn = document.querySelector('.close-btn');
-
-
 
 const openMenu = () => {
   sideMenu.classList.add('open-side-menu');
@@ -22,23 +20,23 @@ const showModalDesc = (card) => {
   const title = card.querySelector('.project-title').textContent;
   const description = card.querySelector('.detail-description').textContent;
   const productImageSrc = card.querySelector('.project-image img').src;
-  desc_modal.querySelector('.desc-title').textContent = title;
-  desc_modal.querySelector('.parag').textContent = description;
-  desc_modal.querySelector('.modal-img').src = productImageSrc;
-}
+  descModal.querySelector('.desc-title').textContent = title;
+  descModal.querySelector('.parag').textContent = description;
+  descModal.querySelector('.modal-img').src = productImageSrc;
+};
 
 const closeModalDesc = () => {
   detailBackdrop.classList.remove('open-backdrop');
-}
+};
 
 menuItems.forEach((el) => {
   el.addEventListener('click', () => closeMenu());
 });
 
-//for each project card 
-projectCard.forEach((card, index) => {
+// for each project card
+projectCard.forEach((card) => {
   card.querySelector('.project-button-link-container a').addEventListener('click', () => showModalDesc(card));
-})
+});
 
 // events
 menubtn.addEventListener('click', () => openMenu());
